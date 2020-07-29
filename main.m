77cc02f4904c549a30780ab700319245e067f2a4
JITHIN K C <jithinkc22@gmail.com>
2020-07-28 23:36:28 -1000

    Update main.m

--- main.m
+++ main.m
@@ -8,10 +8,8 @@
      
            % If you are using my code for your system or project, you should always cite my paper as a reference. 
      
-           % 1. K C.Jithin, SyamSankar, "Colour image encryption algorithm combining Arnold map, DNA sequence operation, and a Mandelbrot set", 
-           	% Journal of Information Security and Applications, Elsevier, Volume 50, February 2020, DOI: https://doi.org/10.1016/j.jisa.2019.102428
-           
-	  % 2. A. Alarifi, S. Sankar, T. Altameem, K. C. Jithin, M. Amoon and W. El-Shafai, "A Novel Hybrid Cryptosystem for Secure Streaming of High  			% Efficiency H.265 Compressed Videos in IoT Multimedia Applications," in IEEE Access, doi: 10.1109/ACCESS.2020.3008644.
+            web('https://docs.google.com/document/d/1AbCxFoUhdOCppM8novgCdOv0F9mqYe7HlBU7yX7Svx0/edit?usp=sharing')
+            web('published_work.html')
            
 % -----------------------------------------------------------------------------------------------------------------------------------   
     % Input the image name here. - you can choose image from our image-dataset
